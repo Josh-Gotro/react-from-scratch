4e1d016f50a87c32d022aa689b23bb3096183edd
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-09-13 15:54:18 -0500

    create webpack, redirect to src file so webpack has correct pathing, create webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            { 
+                test: /\.(js)$/, 
+                use: 'babel-loader' 
+            },
+            { 
+                test: /\.css$/, 
+                use: ['style-loader', 'css-loader'] 
+            }
+        ]
+    },
+    mode: 'development',
+    plugins: [
+
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
+}
+
